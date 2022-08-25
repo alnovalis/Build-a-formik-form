ac794f9bbda1ca58458e7208940d7bbf6fc7a895
Ashlea Novalis <104088564+alnovalis@users.noreply.github.com>
2022-08-25 06:00:25 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Formik } from "formik";
+
+const FormikExample = () => {
+  return (
+    <div>
+      <h1>Sign-up Here</h1>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = "Field Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Username should be an email";
+          } else if (!values.password) {
+            errors.password = "Password Required";
+          } else if (values.password.length > 15) {
+            errors.password = "Must be 15 characters or less";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default FormikExample;
